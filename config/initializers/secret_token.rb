79cfdfc06736b842ef245543fcaccb0eaa23d476
sunil7300 <bittu7300@gmail.com>
2014-12-14 18:35:16 -0800

    myfirstcommit84260

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Syam::Application.config.secret_token = 'ac63522c01e07edc95913aa22143d2950701792d8786b93ec6306d2da02501850e86d71a6e2709049b1b6ffcfdc5ac87c6d3ab76c659e38e55d24ee6a48449b6'
